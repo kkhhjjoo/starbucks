06598e9854d86dd4915c3f2a0f706d99ea18062a
hyunjoo <khjoo1203@gmail.com>
2025-08-11 12:50:56 +0900

    프로모션 이미지 슬라이드

--- js/main.js
+++ js/main.js
@@ -53,3 +53,23 @@ new Swiper('.notice-line .swiper-container', {
   autoplay: true,
   loop: true,
 });
+
+new Swiper('.promotion .swiper-container', {
+      direction: 'horizontal',
+      slidesPerView: 3, // 한번에 보여줄 슬라이드 갯수
+      spaceBetween: 10, // 슬라이드 사이 여백
+      centeredSlides: true, // 1번 슬라이드가 가운데 보이게
+      loop: true,
+      autoplay: {
+        delay: 5000
+      },
+
+      pagination: {
+        el: '.promotion .swiper-pagination', //페이지 번호 요소 선택자
+        clickable: true,
+      },
+      navigation: {
+        prevEl: '.promotion .swiper-prev',
+        nextEl: '.promotion .swiper-next'
+      }
+});
